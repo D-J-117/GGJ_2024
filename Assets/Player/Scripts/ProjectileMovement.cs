ef275bbf022288e7c62900096640226f4d3dc752
Dan J <daniel8.jenkins@live.uwe.ac.uk>
2024-01-27 15:47:40 +0000

    Added random projectile sprites. Updated grounded check to use multiple raycasts

--- Assets/Player/Scripts/ProjectileMovement.cs
+++ Assets/Player/Scripts/ProjectileMovement.cs
@@ -9,6 +9,15 @@ public class ProjectileMovement : MonoBehaviour
     public Vector3 direction = Vector3.right;
     public float speed = 10;
     private bool first_frame = true;
+
+    [SerializeField] private List<Sprite> sprites;
+
+    private void Start()
+    {
+        int x = Random.Range(0, sprites.Count);
+        transform.GetComponent<SpriteRenderer>().sprite = sprites[x];
+    }
+
     private void Update()
     {
         transform.position = transform.position + direction * speed * Time.deltaTime;
