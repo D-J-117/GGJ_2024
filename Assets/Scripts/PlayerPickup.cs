f585f3287ca1a82dfd934a0067c971414c121291
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2024-01-27 16:25:53 +0000

    implemented collecting balloon animals

--- Assets/Scripts/PlayerPickup.cs
+++ Assets/Scripts/PlayerPickup.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerPickup : MonoBehaviour
 {
+    int score = 0;
+    [SerializeField] TMP_Text score_text;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "balloon_animal")
         {
-
+            score++;
+            score_text.text = "Score: " + score.ToString();
+            Destroy(collision.gameObject);
         }
     }
 }
