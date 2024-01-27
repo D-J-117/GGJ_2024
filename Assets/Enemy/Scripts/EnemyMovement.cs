20a466c4c1ae7e94c4626b883e8dd81cde0849d3
Charlie Carter <Charlie4.Carter@live.uwe.ac.uk>
2024-01-27 13:47:44 +0000

    Enemy Patrol

--- Assets/Enemy/Scripts/EnemyMovement.cs
+++ Assets/Enemy/Scripts/EnemyMovement.cs
@@ -5,17 +5,58 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject pointA;
+    public GameObject pointB;
+    public Rigidbody2D rigid;
+
     [SerializeField]
     private float speed = 2f;
+    private bool collide = false;
+    private Transform currentPos;
 
-    private void Update()
+    void Start()
     {
-        Enemy_Movement();
+        rigid = GetComponent<Rigidbody2D>();
+        currentPos = pointB.transform;
     }
 
-    void Enemy_Movement()
+    void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if (collide && (rigid.velocity.x == 1))
+        {
+            rigid.velocity = new Vector2(-speed, 0);
+        }
+        if (collide && (rigid.velocity.x == -1))
+        {
+            rigid.velocity = new Vector2(speed, 0);
+        }
+
+        Vector2 point = currentPos.position - transform.position;
+        if (currentPos == pointB.transform)
+        {
+            rigid.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rigid.velocity = new Vector2 (-speed, 0);
+        } 
+            
+        if (Vector2.Distance(transform.position, currentPos.position) < 0.5f && currentPos == pointB.transform)
+        {
+            currentPos = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPos.position) < 0.5f && currentPos == pointA.transform)
+        {
+            currentPos = pointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,7 +64,18 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             Debug.Log("Enter");
-            transform.Translate(-Vector2.left * speed * Time.deltaTime);
+            if (!collide)
+            {
+                collide = true;
+                Debug.Log(collide);
+            }
+            else
+            {
+                collide = false;
+                Debug.Log(collide);
+            }
+           
+
             // damage function call within player.
         }
     }
